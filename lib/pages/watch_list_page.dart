b371920d07b7562bce0abddb1896b316a2c91699
shofiq <shofiqulislam0023@gmail.com>
2024-05-10 01:24:58 +0600

    extracted some widget to method

--- lib/pages/watch_list_page.dart
+++ lib/pages/watch_list_page.dart
@@ -39,91 +39,101 @@ class _WatchListPageState extends State<WatchListPage> {
         /// The search bar
         Container(
           padding: const EdgeInsets.only(bottom: 10.0, left: 10.0, right: 10.0),
-          child: TextField(
-            controller: searchBarController,
-            textCapitalization: TextCapitalization.sentences,
-            decoration: InputDecoration(
-              label: const Text(
-                "Search...",
-                style: TextStyle(
-                  color: Colors.white54,
-                  fontSize: 14.0,
-                ),
-              ),
-              suffixIcon: searchBarController.text.isNotEmpty
-                  ? IconButton(
-                      onPressed: () {
-                        clearSarchBar();
-                      },
-                      icon: const Icon(
-                        Icons.cancel_rounded,
-                        size: 26.0,
-                        color: Colors.white70,
-                      ),
-                    )
-                  : null,
-            ),
-            style: const TextStyle(
-              color: Colors.white,
-            ),
-            onChanged: (value) {
-              if (value.isEmpty) {
-                context.read<WatchListDatabase>().clearFilter();
-              } else {
-                context.read<WatchListDatabase>().filterList(value);
-              }
-            },
-          ),
+          child: getSearchBar(context),
         ),
 
         /// The List of Shows
         Expanded(
-          child: lists.isNotEmpty
-              ? AnimationLimiter(
-                  child: ListView.builder(
-                    physics: const BouncingScrollPhysics(
-                      parent: AlwaysScrollableScrollPhysics(),
-                    ),
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.only(
-                        left: 8.0, right: 8.0, top: 5.0, bottom: 60.0),
-                    itemCount: lists.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return AnimationConfiguration.staggeredList(
-                        position: index,
-                        delay: const Duration(milliseconds: 100),
-                        child: SlideAnimation(
-                          duration: const Duration(milliseconds: 2500),
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          child: FadeInAnimation(
-                            curve: Curves.fastLinearToSlowEaseIn,
-                            duration: const Duration(milliseconds: 2500),
-                            child: WatchListItem(
-                              watchListModel: lists[index],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )
-              : const Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "No shows found.",
-                        style: TextStyle(color: Colors.white70, fontSize: 24.0),
-                      ),
-                      Text(
-                        " Why don't you add one?",
-                        style: TextStyle(color: Colors.white70, fontSize: 24.0),
-                      ),
-                    ],
-                  ),
-                ),
+          child: lists.isNotEmpty ? getList(lists) : getNoDataText(),
         ),
       ],
+    );
+  }
+
+  TextField getSearchBar(BuildContext context) {
+    return TextField(
+      controller: searchBarController,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        label: const Text(
+          "Search...",
+          style: TextStyle(
+            color: Colors.white54,
+            fontSize: 14.0,
+          ),
+        ),
+        suffixIcon: searchBarController.text.isNotEmpty
+            ? IconButton(
+                onPressed: () {
+                  clearSarchBar();
+                },
+                icon: const Icon(
+                  Icons.cancel_rounded,
+                  size: 26.0,
+                  color: Colors.white70,
+                ),
+              )
+            : null,
+      ),
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+      onChanged: (value) {
+        if (value.isEmpty) {
+          context.read<WatchListDatabase>().clearFilter();
+        } else {
+          context.read<WatchListDatabase>().filterList(value);
+        }
+      },
+    );
+  }
+
+  Padding getNoDataText() {
+    return const Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        children: [
+          Text(
+            "No shows found.",
+            style: TextStyle(color: Colors.white70, fontSize: 24.0),
+          ),
+          Text(
+            " Why don't you add one?",
+            style: TextStyle(color: Colors.white70, fontSize: 24.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AnimationLimiter getList(List<WatchListModel> lists) {
+    return AnimationLimiter(
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+        shrinkWrap: true,
+        padding: const EdgeInsets.only(
+            left: 8.0, right: 8.0, top: 5.0, bottom: 60.0),
+        itemCount: lists.length,
+        itemBuilder: (BuildContext context, int index) {
+          return AnimationConfiguration.staggeredList(
+            position: index,
+            delay: const Duration(milliseconds: 100),
+            child: SlideAnimation(
+              duration: const Duration(milliseconds: 2500),
+              curve: Curves.fastLinearToSlowEaseIn,
+              child: FadeInAnimation(
+                curve: Curves.fastLinearToSlowEaseIn,
+                duration: const Duration(milliseconds: 2500),
+                child: WatchListItem(
+                  watchListModel: lists[index],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
